6d74cdd35458b11a8a3113c8cd28d6781afffb86
rolando926 <rolando_colon@homedepot.com>
2017-04-27 16:34:31 -0400

    Initial commit for Palindrome exercice - Reversing string.

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome{
+	public static void main(String[] args){
+		String str = "tacocat";
+		String str2 = "";
+
+		//idea #1: reverse it and save it in different variable, 
+		//then compare it
+
+		for(int i = str.length() - 1; i >= 0; i--){
+			str2 = str2 + str.charAt(i);
+		}
+
+		if(str.equals(str2)){
+			System.out.println("Is Palindrome!");
+		}
+		else{
+			System.out.println("Not a Palindrome!");
+		}
+
+	}
+
+}
